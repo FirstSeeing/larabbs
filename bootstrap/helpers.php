f8b4b344df98cabb1473738b04dd4a4d7d4b82f6
FirstSight <firstsight@wxxk.cc>
2018-07-23 11:51:28 +0000

    Procfile for Heroku

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,5 +3,10 @@
  * @Author: Marte
  * @Date:   2018-07-22 14:01:50
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-07-22 14:22:02
+ * @Last Modified time: 2018-07-22 15:00:18
  */
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
