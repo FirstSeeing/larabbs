6b94cf8350f798eab0e65b74ad8e1202d684fee4
FirstSight <firstsight@wxxk.cc>
2018-07-27 13:21:13 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,10 +3,16 @@
  * @Author: Marte
  * @Date:   2018-07-22 14:01:50
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-07-22 15:00:18
+ * @Last Modified time: 2018-07-27 21:12:12
  */
 
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
